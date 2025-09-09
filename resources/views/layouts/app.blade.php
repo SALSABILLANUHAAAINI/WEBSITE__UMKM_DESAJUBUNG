d29a99872e0863abe3ff88cd51bd47910cfb0ad7
Favian Rifqi Wibowo <rifqi191104vian@gmail.com>
2025-09-09 16:47:01 +0700

    perubahan untuk layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,36 +3,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'Home')</title>
+    <title>@yield('title', 'UMKM Desa Jubung')</title>
 
-    {{-- CSS global --}}
-    <link rel="stylesheet" href="{{ asset('css/index.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/user/app.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/admin/login.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/admin/sidebar.css') }}">
+    {{-- Memanggil Bootstrap dari CDN sebagai fondasi dasar styling --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
+    {{-- Memanggil file CSS global utama Anda. Pastikan file ini ada. --}}
+    <link rel="stylesheet" href="{{ asset('css/index.css') }}">
 
-    {{-- JS global --}}
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    {{-- CSS tambahan khusus halaman --}}
+    {{-- Slot ini akan diisi oleh CSS spesifik dari halaman lain (seperti halaman katalog, dll) --}}
     @yield('styles')
 </head>
 <body>
 
-    {{-- Navbar --}}
+    {{-- Memuat komponen Navbar --}}
     @include('user.partials.navbar')
 
-    {{-- Konten utama --}}
-    <main >
+    {{-- Konten utama dari setiap halaman akan dimuat di sini --}}
+    <main>
         @yield('content')
     </main>
 
-    {{-- Footer --}}
+    {{-- Memuat komponen Footer --}}
     @include('user.partials.footer')
 
-    {{-- Script tambahan khusus halaman --}}
+    {{-- Slot untuk JavaScript spesifik dari halaman lain --}}
     @yield('scripts')
 
 </body>
