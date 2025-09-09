298e028cbe16c12f1b6571c0863abf9ade9a5be9
Favian Rifqi Wibowo <rifqi191104vian@gmail.com>
2025-09-09 17:05:54 +0700

    perubahan untuk layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,20 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'Home')</title>
+    <title>@yield('title', 'UMKM Desa Jubung')</title>
 
     {{-- CSS global --}}
-    <link rel="stylesheet" href="{{ asset('css/index.css') }}">
+    {{-- Pastikan urutan ini benar. app.css harus memuat semua style lain --}}
     <link rel="stylesheet" href="{{ asset('css/user/app.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/admin/login.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/admin/sidebar.css') }}">
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"> -->
+    
+    {{-- File CSS admin tidak perlu dimuat di halaman user --}}
+    {{-- <link rel="stylesheet" href="{{ asset('css/admin/login.css') }}"> --}}
+    {{-- <link rel="stylesheet" href="{{ asset('css/admin/sidebar.css') }}"> --}}
 
 
     {{-- JS global --}}
     <script src="{{ asset('js/app.js') }}" defer></script>
 
-    {{-- CSS tambahan khusus halaman --}}
+    {{-- CSS tambahan khusus halaman (jika ada) --}}
     @yield('styles')
 </head>
 <body>
@@ -25,14 +26,14 @@
     @include('user.partials.navbar')
 
     {{-- Konten utama --}}
-    <main >
+    <main class="main-content">
         @yield('content')
     </main>
 
     {{-- Footer --}}
     @include('user.partials.footer')
 
-    {{-- Script tambahan khusus halaman --}}
+    {{-- Script tambahan khusus halaman (jika ada) --}}
     @yield('scripts')
 
 </body>
