f858c00d212d9c59f0ca761a32059c25d8bd9ca2
Favian Rifqi Wibowo <rifqi191104vian@gmail.com>
2025-09-09 17:11:00 +0700

    perubahan untuk layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
     {{-- File CSS admin tidak perlu dimuat di halaman user --}}
     {{-- <link rel="stylesheet" href="{{ asset('css/admin/login.css') }}"> --}}
     {{-- <link rel="stylesheet" href="{{ asset('css/admin/sidebar.css') }}"> --}}
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"> -->
 
 
     {{-- JS global --}}
