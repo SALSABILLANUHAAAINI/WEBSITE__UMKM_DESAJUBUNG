c08c535d055a4e8bd8d97b50fb8df23163dbcc39
Favian Rifqi Wibowo <rifqi191104vian@gmail.com>
2025-09-11 00:12:55 +0700

    memperbaiki navbar supaya responsif

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     {{-- CSS global --}}
     {{-- Pastikan urutan ini benar. app.css harus memuat semua style lain --}}
     <link rel="stylesheet" href="{{ asset('css/user/app.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/navbar.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/partials/navbar.css') }}">
 <script src="{{ asset('js/navbar.js') }}" defer></script>
 
     {{-- File CSS admin tidak perlu dimuat di halaman user --}}
