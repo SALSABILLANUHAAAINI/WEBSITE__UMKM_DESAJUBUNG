b1b95c684b2f1f899c4442689744750a8c042905
Claudiapurba <claudiapurba30@gmail.com>
2025-09-10 22:52:35 +0700

    edit footer

--- resources/views/user/partials/footer.blade.php
+++ resources/views/user/partials/footer.blade.php
@@ -2,10 +2,9 @@
     <h2 class="footer-brand">UMKM Desa Jubung</h2>
 
     <div class="socials">
-        <a href="#"><i class="fab fa-instagram"></i></a>
-        <a href="#"><i class="fab fa-facebook"></i></a>
+        <a href="https://www.instagram.com/desa_jubung?igsh=MXc3M3E4b21xdXZnaw=="><i class="fab fa-instagram"></i></a>
+        <a href="https://www.facebook.com/pages/Desa-Jubung/1730040947247226/"><i class="fab fa-facebook"></i></a>
         <a href="https://www.youtube.com/watch?v=s1H20qu8-Ak&t=1s"><i class="fab fa-youtube"></i></a>
-        <a href="mailto:umkm.jubung@example.com"><i class="fas fa-envelope"></i></a>
     </div>
 
     <p class="copyright">&copy; {{ date('Y') }} UMKM Desa Jubung. All rights reserved.</p>
